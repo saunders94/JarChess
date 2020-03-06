2bc3d611dd8f21da750c7b8232f475b7c28a2193
Joshua Zierman <py1422xs@metrostate.edu>
2020-03-06 03:22:28 -0600

    added some javadocs

--- app/src/main/java/com/example/jarchess/match/datapackage/Datapackege.java
+++ app/src/main/java/com/example/jarchess/match/datapackage/Datapackege.java
@@ -1,19 +1,37 @@
 package com.example.jarchess.match.datapackage;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.jarchess.match.move.CastleMove;
 import com.example.jarchess.match.move.Move;
 import com.example.jarchess.match.move.StandardMove;
 import com.example.jarchess.match.turn.Turn;
 
+/**
+ * A datapackage is a package that contains data to be sent from one remote opponent and recieved by another.
+ * <p>
+ * They can contain the following:
+ * <p><ul>
+ * <li>A turn
+ * <li>A resignation
+ * <li>A pause request
+ * <li>A pause approval
+ * <li>A pause rejection
+ * </ul>
+ */
 public class Datapackege {
     private final DatapackageType datapackageType;
 
+    /**
+     * Creates a Datapackage from a Turn object.
+     *
+     * @param turn the turn to be packaged
+     */
     public Datapackege(@NonNull Turn turn) {
         this.turn = turn;
 
-        if(turn.getMove() == null){
+        if (turn.getMove() == null) {
             throw new IllegalStateException("unexpected null move contained in a turn");
         }
 
@@ -29,21 +47,26 @@ public class Datapackege {
 
     private final Turn turn;
 
-    public Datapackege(DatapackageType datapackageType) {
-        this.datapackageType = datapackageType;
-        this.turn = null;
-    }
-
+    /**
+     * Gets the type of datapackage this is.
+     * @return the type of datapackage this is
+     */
     public DatapackageType getDatapackageType() {
         return datapackageType;
     }
 
+    /**
+     * Gets the turn packaged in this.
+     *
+     * @return the turn packaged in this, may be null.
+     */
+    @Nullable
     public Turn getTurn() {
         return turn;
     }
 
     public enum DatapackageType {
-        STANDARD_MOVE_TURN(0),
+        STANDARD_MOVE_TURN(0), // Do we need or want both of these Turn types or should we just have one called TURN? TODO
         CASTLE_MOVE_TURN(1),
         RESIGNATION(2),
         PAUSE_REQUEST(3),
