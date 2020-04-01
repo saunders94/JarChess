36d331c992beaa51772a328b178ea8c856953dcb
Joshua Zierman <py1422xs@metrostate.edu>
2020-04-01 17:11:30 -0500

    rename ClearableManager to MatchClearableManager

--- app/src/main/java/com/example/jarchess/match/events/PauseButtonPressedEventManager.java
+++ app/src/main/java/com/example/jarchess/match/events/PauseButtonPressedEventManager.java
@@ -14,7 +14,7 @@ public class PauseButtonPressedEventManager implements Clearable {
      */
     private PauseButtonPressedEventManager() {
         super();
-        ClearableManager.add(this);
+        MatchClearableManager.add(this);
     }
 
     /**
