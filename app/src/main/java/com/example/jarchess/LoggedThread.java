a7c32d9995da8b381f2daf01ee6b8db2b806d206
Joshua Zierman <py1422xs@metrostate.edu>
2020-04-24 20:34:30 -0500

    enhancements to LoggingThread class
    - allows a logging of all threads (shows alive/dead and interupted status)
    - allows a logging of all threads that hold a lock (might not show accurate info (since we can't get that lock to make it thread safe)

--- app/src/main/java/com/example/jarchess/LoggedThread.java
+++ app/src/main/java/com/example/jarchess/LoggedThread.java
@@ -2,8 +2,14 @@ package com.example.jarchess;
 
 import android.util.Log;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 public class LoggedThread extends Thread {
+    private static Collection<LoggedThread> list = new LinkedList<>();
+    private final String TAG;
     public LoggedThread(final String TAG, final Runnable runnable, final String threadName) {
+
         super(new Runnable() {
             @Override
             public void run() {
@@ -15,5 +21,41 @@ public class LoggedThread extends Thread {
                 }
             }
         }, threadName);
+        this.TAG = TAG;
     }
+
+    public static void clear() {
+        list.clear();
+    }
+
+    public static void logAllThreads() {
+        Collection<LoggedThread> toRemove = new LinkedList<>();
+        for (LoggedThread loggedThread : list) {
+
+            String interrupted = loggedThread.isInterrupted() ? " and Interrupted" : "";
+
+            if (loggedThread.isAlive()) {
+                Log.d(loggedThread.TAG, "logAllThreads: " + loggedThread.getName() + " is Alive" + interrupted);
+
+            } else {
+                Log.d(loggedThread.TAG, "logAllThreads: " + loggedThread.getName() + " is Dead" + interrupted);
+            }
+        }
+    }
+
+
+    public static void logThreadHoldingLock(Object lock, String lockName) {
+        for (LoggedThread loggedThread : list) {
+            if (holdsLock(lock)) {
+                Log.d(loggedThread.TAG, "logAllThreads: " + loggedThread.getName() + " is holding lock " + lockName);
+            }
+        }
+    }
+
+    @Override
+    public synchronized void start() {
+        list.add(this);
+        super.start();
+    }
+
 }
