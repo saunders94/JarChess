dc25197828142ad0054822f2ff9ce71fff8f9cae
Joshua Zierman <joshuajzierman@hotmail.com>
2020-05-01 11:41:56 -0500

    Revert "Fixed issue #2"

--- app/src/main/java/com/example/jarchess/match/PlayerMatch.java
+++ app/src/main/java/com/example/jarchess/match/PlayerMatch.java
@@ -159,7 +159,7 @@ public class PlayerMatch extends Match {
             // we don't need to check for agreement
             if (!matchClock.isRunning()) {
                 matchClock.resume();
-                matchView.hidePendingResumeDialog();
+                matchView.hidePendingDrawDialog();
             }
         } else if (opponent instanceof RemoteOpponent) {
             Log.i(TAG, "handlePlayerResumeRequest: remote opponent detected");
