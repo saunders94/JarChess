2e1b42f27a254d530d7a6a8ef920414d5e9f20b7
Joshua Zierman <github@zierman.us>
2021-09-17 22:43:26 -0500

    Working on allowing for asynchronous sending and receiving of messages.
    See Issue #4
    
    - I made it so two threads are started, one for sending and one for receiving
    - I made GameIO closable
    - I made it so the socket and data streams get closed when GameIO closes
    - I made it so the socket and data streams get closed if there is a failure starting things.
    - I made it so the GameIO will close if the socket connection ends.

--- app/src/main/java/com/example/jarchess/online/networking/GameIO.java
+++ app/src/main/java/com/example/jarchess/online/networking/GameIO.java
@@ -2,6 +2,7 @@ package com.example.jarchess.online.networking;
 
 import android.util.Log;
 
+import com.example.jarchess.LoggedThread;
 import com.example.jarchess.RemoteOpponentInfoBundle;
 import com.example.jarchess.jaraccount.JarAccount;
 import com.example.jarchess.online.datapackage.Datapackage;
@@ -12,13 +13,31 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.Closeable;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class GameIO {
+/**
+ * A self running Input/Output system that allows for asynchronous input and output.
+ */
+public class GameIO implements Closeable {
+
+    /**
+     * The timeout constant of 300000 which represents 5 minutes
+     */
+    public static final int TIMEOUT = 300000;
+
+    private static final String PAYLOAD_KEY = "payload";
+    private static final String MATCH_COMMUNICATION = "MatchCommunication";
+    private static final String TYPE_KEY = "type";
+    private static final Datapackage.DatapackageJSONConverter JSON_TO_DATAPACKAGE_CONVERTER = Datapackage.DatapackageJSONConverter.getInstance();
     private final String TAG = "GameIO";
+    private final LoggedThread senderThread;
+    private final LoggedThread receiverThread;
+    private boolean isAlive = false;
+    private boolean isDying = false;
     private String gameToken;
     private String gameServer = "AppLB-f1eb9121f64bbd52.elb.us-east-2.amazonaws.com";
     private String serverIp = "3.18.79.149";
@@ -28,112 +47,223 @@ public class GameIO {
     private DataInputStream in;
     private DataOutputStream out;
     private RemoteOpponentInfoBundle remoteOpponentInfoBundle;
+    private Closeable[] closeables;
 
-
+    /**
+     * Creates a GameIO object, which will connect to the server and begin sending and receiving available messages.
+     *
+     * @param datapackageQueue         the datapackage input and output queueing object.
+     * @param gameToken                the token representing the game this GameIO is being used for
+     * @param remoteOpponentInfoBundle the opponent's information
+     */
     public GameIO(DatapackageQueue datapackageQueue, String gameToken,
-                  RemoteOpponentInfoBundle remoteOpponentInfoBundle) throws IOException {
+                  RemoteOpponentInfoBundle remoteOpponentInfoBundle) {
+
+        boolean canStart = false;
         this.datapackageQueue = datapackageQueue;
         this.remoteOpponentInfoBundle = remoteOpponentInfoBundle;
         this.gameToken = gameToken;
-        this.socket = new Socket(serverIp, serverPort);
-        this.socket.setSoTimeout(300000);
+        try {
+            this.socket = new Socket(serverIp, serverPort);
+            this.socket.setSoTimeout(TIMEOUT);
 
-        this.in = new DataInputStream(
-                new BufferedInputStream(
-                        socket.getInputStream()));
-        this.out = new DataOutputStream(
-                new BufferedOutputStream(
-                        socket.getOutputStream()));
+            this.in = new DataInputStream(
+                    new BufferedInputStream(
+                            socket.getInputStream()));
+
+            this.out = new DataOutputStream(
+                    new BufferedOutputStream(
+                            socket.getOutputStream()));
+
+            canStart = true;
+
+        } catch (IOException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+            try {
+                in.close();
+            } catch (IOException ex) {
+                // just continue
+            }
+            try {
+                out.close();
+            } catch (IOException ex) {
+                // just continue
+            }
+            try {
+                socket.close();
+            } catch (IOException ex) {
+                // just continue;
+            }
+        }
+
+        receiverThread = new LoggedThread(TAG, new Runnable() {
+            @Override
+            public void run() {
+                while (!isDone()) {
+                    try {
+                        receiveNext();
+                    } catch (IOException e) {
+                        Log.e(TAG, e.getLocalizedMessage());
+                        // We log the error message and continue
+                    } catch (JSONException e) {
+                        Log.e(TAG, e.getLocalizedMessage());
+                        // We log the error message and continue
+                    }
+                }
+                synchronized (GameIO.this) {
+                    if (!isDying && isAlive) {
+                        close();
+                    }
+                }
+            }
+        }, "GameIO-Receiver-Thread");
+
+        senderThread = new LoggedThread(TAG, new Runnable() {
+            @Override
+            public void run() {
+                while (!isDone()) {
+                    try {
+                        sendNext();
+                    } catch (IOException e) {
+                        Log.e(TAG, e.getLocalizedMessage());
+                        // We log the error message and continue
+                    } catch (JSONException e) {
+                        Log.e(TAG, e.getLocalizedMessage());
+                        // We log the error message and continue
+                    }
+                }
+                synchronized (GameIO.this) {
+                    if (!isDying && isAlive) {
+                        close();
+                    }
+                }
+            }
+        }, "GameIO-Sender-Thread");
+
+        if (canStart) {
+            closeables = new Closeable[]{in, out, socket};
+            this.start();
+        }
+    }
+
+    /**
+     * Close the GameIO object, terminating the connection with the server and stops sending and receiving messages.
+     * <p>
+     * This is a non-blocking call, and it is possible that messages will be sent or received shortly
+     * after this method is called.
+     */
+    public void close() {
+        Log.i(TAG, "closing GameIO");
 
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                try {
-                    processMoves();
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
+
+                // signal that the GameIO is dying
+                synchronized (GameIO.this) {
+                    isDying = true;
                 }
+
+                try { // wait for the receiver thread to end
+                    if (receiverThread != null && receiverThread.isAlive()) {
+                        receiverThread.join();
+                    }
+                } catch (InterruptedException e) {
+                    // just continue
+                }
+                try {// wait for the sender thread to end
+                    if (senderThread != null && senderThread.isAlive()) {
+                        senderThread.join();
+                    }
+                } catch (InterruptedException e) {
+                    // just continue
+                }
+                synchronized (GameIO.this) {
+
+                    // close all the stuff we need to close
+                    for (Closeable c : closeables) {
+                        try {
+                            c.close();
+                        } catch (IOException e) {
+                            // just keep moving along
+                        }
+                    }
+
+                    // signal that the GameIO is dead
+                    isDying = false;
+                    isAlive = false;
+                }
+
+                Log.i(TAG, "Done closing GameIO");
             }
         };
-        Thread t = new Thread(r);
-        t.start();
+        new LoggedThread(TAG, r, "GameIO-KillThread").start();
     }
 
-    private void processMoves() throws IOException, JSONException {
-        Log.d(TAG, "processMoves() called");
-        Log.d(TAG, "processMoves is running on thread: " + Thread.currentThread().getName());
+    private synchronized boolean isDone() {
+        return isDying || socket == null || !socket.isConnected();
+    }
 
-        //socket = new Socket(gameServer, serverPort);
-
-        String response;
+    private void receiveNext() throws JSONException, IOException {
+        int bytesRead;
+        StringBuilder msgBuilder = new StringBuilder();
         final byte[] buffer = new byte[1024];
-        Log.i(TAG, "Socket :" + socket.toString() + "   in: " + in + "      out: " + out);
-        JSONObject initialObject = new JSONObject();
-        Log.i(TAG, "remoteOpponentInfoBundle Color = " + remoteOpponentInfoBundle.getColor().toString());
-        if (remoteOpponentInfoBundle.getColor().toString().equals("WHITE")) {
-            JSONObject jsonObj = new JSONObject();
-            Log.i(TAG, "Initial move - waiting for oponent to move");
-            jsonObj.put("requestType", "MakeMove");
-            jsonObj.put("username", JarAccount.getInstance().getName());
-            jsonObj.put("game_token", gameToken);
-            jsonObj.put("signon_token", JarAccount.getInstance().getSignonToken());
-            jsonObj.put("move", "black");
-            out.writeUTF(jsonObj.toString());
-            out.flush();
-            int resp = in.read(buffer);
-            String respString = new String(buffer).trim();
-            Log.i(TAG, "ResponseStr: " + respString);
-            JSONObject responseObject = new JSONObject(respString);
-            datapackageQueue.insertClientBoundDatapackageQueue(
-                    Datapackage.DatapackageJSONConverter.getInstance().convertFromJSONObject(responseObject));
-        } else {
-            JSONObject jsonObj = new JSONObject();
-            jsonObj.put("requestType", "MakeMove");
-            jsonObj.put("username", JarAccount.getInstance().getName());
-            jsonObj.put("game_token", gameToken);
-            jsonObj.put("signon_token", JarAccount.getInstance().getSignonToken());
-            jsonObj.put("move", "white");
-            out.writeUTF(jsonObj.toString());
-            out.flush();
-        }
-        while (true) {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("requestType", "MakeMove");
-            jsonObject.put("username", JarAccount.getInstance().getName());
-            jsonObject.put("game_token", gameToken);
-            jsonObject.put("signon_token", JarAccount.getInstance().getSignonToken());
-            Log.i(TAG, "JsonObject: " + jsonObject.toString());
-            Log.i(TAG, "getClientBoundDatapackage");
-            Datapackage datapackage = datapackageQueue.getServerBoundDatapackage();
-            Log.i(TAG, "processMoves: datapackage received = " + datapackage);
-            if (datapackage != null) {
-                jsonObject.put("move", datapackage.getJSONObject());
-                out.writeUTF(jsonObject.toString());
-                out.flush();
-            }
 
-            Log.i(TAG, "JsonObject: " + jsonObject.toString());
-            Log.i(TAG, "sending datapackage");
-            Log.i(TAG, String.valueOf(socket));
-            Log.i(TAG, "waiting on IO");
-            Integer resp = null;
-            try {
-                while (resp == null) {
-                    resp = in.read(buffer);
-                }
-            } catch (IOException e) {
-                // just keep trying
-            }
-            String respSting = new String(buffer).trim();
-            if (respSting.isEmpty()) {
+        do {
+            bytesRead = in.read(buffer);
+            msgBuilder.append(buffer);
+        } while (!isDone() && bytesRead != -1);
+        String msg = msgBuilder.toString().trim();
+        if (msg.isEmpty() || isDone()) {
+            return;
+        }
+        Log.i(TAG, "msg: " + msg);
+        JSONObject messageObject = new JSONObject(msg);
+
+        switch (messageObject.getString(TYPE_KEY)) {
+            case MATCH_COMMUNICATION:
+                JSONObject payload = messageObject.getJSONObject(PAYLOAD_KEY);
+                datapackageQueue.insertClientBoundDatapackageQueue(
+                        JSON_TO_DATAPACKAGE_CONVERTER.convertFromJSONObject(payload));
+
+                Log.i(TAG, "inserted in to Client Bound Datapackage Queue");
                 break;
-            }
-            Log.i(TAG, "Response: " + respSting);
-            Log.i(TAG, "insertClientBoundDatapackageQueue");
-            JSONObject responseObject = new JSONObject(respSting);
-            datapackageQueue.insertClientBoundDatapackageQueue(
-                    Datapackage.DatapackageJSONConverter.getInstance().convertFromJSONObject(responseObject));
+
+            default:
+                Log.i(TAG, "unexpected message type received: " + messageObject.getString(TYPE_KEY));
+
         }
     }
 
+    private void sendNext() throws JSONException, IOException {
+
+        JSONObject outgoingDatapackageJSON = datapackageQueue.getServerBoundDatapackage().getJSONObject();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("requestType", MATCH_COMMUNICATION);
+        jsonObject.put("username", JarAccount.getInstance().getName());
+        jsonObject.put("game_token", gameToken);
+        jsonObject.put("signon_token", JarAccount.getInstance().getSignonToken());
+        jsonObject.put("payload", outgoingDatapackageJSON);
+        out.writeUTF(jsonObject.toString());
+        out.flush();
+        Log.i(TAG, "JsonObject: " + jsonObject.toString());
+        Log.i(TAG, String.valueOf(socket));
+        Log.i(TAG, "waiting on IO");
+    }
+
+    private void start() {
+
+        Log.i(TAG, "starting GameIO");
+        synchronized (GameIO.this) {
+
+            if (isDying || isAlive) {
+                Log.i(TAG, "GameIO can't start because it is " + (isDying ? "dying" : "already alive"));
+                return; // without starting anything.
+            }
+
+            isAlive = true;
+            receiverThread.start();
+            senderThread.start();
+        }
+    }
 }
